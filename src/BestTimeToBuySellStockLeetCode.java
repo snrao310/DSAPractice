df693c946bff21eebfd65de4a852f3ece11d6bed
Sameeran N Rao <sameerana.nrao@yahoo.com>
2016-10-04 18:51:50 -0700

    Leet code question: Best time to buy sell stock II (many times)
    must add other parts to this code when I come across it.

---
+++ src/BestTimeToBuySellStockLeetCode.java
@@ -0,0 +1,20 @@
+/**
+ * Created by S N Rao on 10/4/2016.
+ */
+public class BestTimeToBuySellStockLeetCode {
+
+    public static int maxProfit(int[] prices) { //Can buy multiple times and sell multiple times.
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String args[]){
+        int prices[]={2,3,1,5,7,2,5};
+        maxProfit(prices);                  //Can buy multiple times and sell multiple times.
+    }
+}
