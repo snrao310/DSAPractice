33eb3518be95d69b0a5efe2c009bc180753d0c90
snrao310 <sameerana.nrao@yahoo.com>
2016-10-29 20:13:01 -0700

    LeetCode Questions:
    1. Count Number of Unique Numbers
    Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
    
    2. Arithmatic Slices
    The function should return the number of arithmetic slices in the array A.

---
+++ src/ArithmaticSlicesLeetCode.java
@@ -0,0 +1,40 @@
+/**
+ * Created by snrao on 10/29/16.
+ */
+public class ArithmaticSlicesLeetCode {
+
+    //finds the all the subsequences that are APs with length at least 3.
+    public static int numberOfArithmeticSlices(int[] A) {
+        if (A.length < 3)
+            return 0;
+
+        int res = 0;
+        int diff = A[1] - A[0];
+        int count = 1;
+        int ndiff;
+
+        for (int i = 2; i < A.length; i++) {
+            ndiff = A[i] - A[i - 1];
+            if (ndiff == diff)
+                count++;
+            else {
+                if (count > 1) {
+                    res += (count * (count - 1) / 2);
+                }
+                count = 1;
+                diff = ndiff;
+            }
+        }
+        if (count > 1) {
+            res += (count * (count - 1) / 2);
+        }
+        return res;
+    }
+
+
+    public static void main(String args[]) {
+        int A[] = {1, 2, 3, 4};
+        int k = numberOfArithmeticSlices(A);
+        System.out.print(k);
+    }
+}
---
+++ src/CountNumbersWithUniqueDigitsLeetCode.java
@@ -0,0 +1,33 @@
+/**
+ * Created by snrao on 10/29/16.
+ */
+public class CountNumbersWithUniqueDigitsLeetCode {
+
+    //counts number of numbers with unique digits between 0 and 10^n.
+    // Uses permutations and dynamic programming.
+    public static int countNumbersWithUniqueDigits(int n) {
+        if(n>11)
+            return 0;
+
+        if(n==0) return 1;
+        if(n==1) return 10;
+        if(n==2) return 91;
+
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=10;
+        dp[2]=91;
+
+        for(int i=3;i<=n;i++){
+            dp[i]=(dp[i-1]-dp[i-2])*(11-i) + dp[i-1];
+        }
+
+        return dp[n];
+    }
+
+
+    public static void main(String args[]) {
+        int k=countNumbersWithUniqueDigits(3);
+        System.out.print(k);
+    }
+}
