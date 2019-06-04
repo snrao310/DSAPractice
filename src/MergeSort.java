db7386496e502996b02a22c596a6b455768efce0
Sriram Vellangallor Subramanian <sriramoman@gmail.com>
2019-06-04 00:45:46 -0700

    Update mergeSort
    Cleaner implementation

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,48 +1,27 @@
 import java.util.Arrays;
-
-/**
- * Created by S N Rao on 3/29/2017.
- */
-public class MergeSort {
-
-    private static void mergeSortFunction(int[] nums){
-        int len=nums.length;
-        if(len==1)
-            return;
-
-        //Divide
-        int[] A= Arrays.copyOfRange(nums,0,len/2);
-        int[] B= Arrays.copyOfRange(nums,len/2,len);
-        mergeSortFunction(A);
-        mergeSortFunction(B);
-
-        //Merge step
-        int j=0,k=0;
-        for(int i=0;i<len;i++){
-            if(A[j]<=B[k])
-                nums[i]=A[j++];
-            else
-                nums[i]=B[k++];
-
-            if(j==A.length){
-                i++;
-                while(k!=B.length)
-                    nums[i++]=B[k++];
-                break;
-            }
-            if(k==B.length){
-                i++;
-                while(j!=A.length)
-                    nums[i++]=A[j++];
-                break;
-            }
-        }
-    }
-
-    public static void main(String args[]){
-        int[] arr={5,3,6,7,2,4,2,1};
-        mergeSortFunction(arr);
-        for(int i: arr)
-            System.out.print(i+" ");
-    }
+//(GPL) Sriram Vellangallor Subramanian
+public class MergeSortDemo {
+	private static void mergeSort(int[] nums) {
+		if(nums.length==1) return;
+		int mid=nums.length/2;
+		//Divide
+		int[] left=Arrays.copyOfRange(nums, 0, mid);
+		int[] right=Arrays.copyOfRange(nums,mid,nums.length);
+		mergeSort(left);
+		mergeSort(right);
+		//Conquer
+		int i=0,j=0,k=0;
+		while(j<left.length&&k<right.length)
+			if(left[j]<=right[k]) nums[i++]=left[j++];
+			else nums[i++]=right[k++];
+		while(j<left.length) nums[i++]=left[j++];
+		while(k<right.length) nums[i++]=right[k++];
+	}
+	
+	public static void main(String[] args) {
+		int[] nums= {8,3,7,1,2};
+		mergeSort(nums);
+		for(int t:nums)
+			System.out.print(t+" ");
+	}
 }
